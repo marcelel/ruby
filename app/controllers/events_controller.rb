c65b833dd90393e39fd07858a3c7abceaeecf0fc
Ubuntu <ubuntu@ip-172-31-85-123.ec2.internal>
2020-01-15 22:20:15 +0000

    event detail

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,8 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show]
+
   def index
+    @events = Event.all
   end
 
   def show
@@ -10,4 +13,15 @@ class EventsController < ApplicationController
 
   def create
   end
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def event_params
+      params.require(:event).permit(:artist, :description, :price_low, :price_high, :date)
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,15 @@
 class Event < ApplicationRecord
-    validates :name, :presence => true, :length => { :minimum => 6 }
-    validates :email_address, :presence => true, format: { with: URI::MailTo::EMAIL_REGEXP } 
-    validates :artist, :presence => true
-    validates :description, :presence => true
-    validates :price_low, :presence => true
-    validates :price_high, :presence => true
-    validates :price_low, :presence => true
+    # validates :artist, 
+    # presenence => true
+
+    # validates :price_low, 
+    # presenence => true,
+    # greater_than:0
+
+    # validates :price_high, 
+    # presenence => true
+    # greater_than:0
+
+    # validates :date, 
+    # presenence => true
 end
